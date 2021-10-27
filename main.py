cc16aa380262d4426e963873a6f2b38fce20c466
Alexey Razmanov <razmanov666>
2021-10-27 15:27:40 +0300

    herokudeploy commit

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import config
 import teams
 
 bot = telebot.TeleBot(config.token)
-all_teams = [teams.liga_bbva, teams.seria_a, teams.ligue_1, teams.bundesliga, teams.barclays]
+all_teams = [
+            teams.liga_bbva
+            # teams.seria_a, 
+            # teams.ligue_1, 
+            # teams.bundesliga, 
+            # teams.barclays
+            ]
 bogma = 426128881
 admin = 406626012
 
 @bot.message_handler(commands=['start', 'help'])
-def cycle(message):
-    for i in list(range(100)):
-        main(message)
-
+# def cycle(message):
+#     for i in list(range(100)):
+#         main(message)
 def main(message):
     liga = random.choice(all_teams)
     team = random.choice(liga)
@@ -60,18 +65,11 @@ def send_logo(team, message):
     if media_url != '':
         print(media_url[-4:]) 
         if media_url[-4:] == '.mp4' or media_url[-4:] == '.gif':
-            bot.send_video(message.chat.id, media_url) 
+            bot.send_video(message.chat.id, media_url)
+        else:
+            bot.send_photo(message.chat.id, media_url)
     else:
         bot.send_message(message.chat.id, text='No logo yet')
-    # elif team == 'Monaco':
-    #     bot.send_video(message.chat.id, 'https://www.gifservice.fr/img/gif-vignette-small/3c10cec4e948d06c5b6c31536ea7f119/549-2014-sports-soccer-club-france-provence-alpes-cote-dazur-as-monaco-2014.gif')
-    
-    # elif team == 'Borussia Dortmund':
-    #     bot.send_photo(message.chat.id, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFl3mabt--U-wPh2nMWWggk18FFXFdGSsAKQ&usqp=CAU')
-    # elif team == 'Barcelona':
-    #     bot.send_video(message.chat.id, 'https://c.tenor.com/HuG2PXbKb98AAAAC/barcelona-fc-barcelona.gif')
-    # elif team == 'Real Madrid':
-    #     bot.send_video(message.chat.id, 'https://i.gifer.com/HD3f.mp4')
 
 
 bot.polling()
